58232cc3f04accd8ca61ab5711094a30f4ad8eb9
BoraY08 <boray@nycstudents.net>
2025-05-03 20:46:27 +0000

    done

--- Data.java
+++ Data.java
@@ -1,7 +1,12 @@
 public class Data {
-    public static final int MAX = /* value not shown */;
+    public static final int MAX = (int)(Math.random()*Integer.MAX_VALUE);
     private int[][] grid;
-
+    public Data(int rows, int columns){
+        grid = new int[rows][columns];
+    }
+    public Data (int[][] arr) {
+        grid = arr;
+    }
 
     /**
      * Fills all elements of grid with randomly generated
@@ -10,7 +15,15 @@ public class Data {
      * grid has at least one element.
      */
     public void repopulate() {
-        /* to be implemented in part (a) */
+        for (int row = 0; row < grid.length; row++){
+            for (int col = 0; col < grid[0].length; col++){
+                int val = 0;
+                while (val % 10 != 0 || val % 100 == 0){
+                    val = (int) (Math.random()* MAX + 1);
+                }
+                grid[row][col] = val;
+            }
+        }
     }
 
 
@@ -22,7 +35,28 @@ public class Data {
      * grid has at least one element.
      */
     public int countIncreasingCols() {
-        /* to be implemented in part (b) */
+        int count = 0;
+        for (int col = 0; col < grid[0].length; col++){
+            int increasing = 1;
+            for (int row = 1; row < grid.length; row++){
+                if (grid[row][col] >= grid[row - 1][col]){
+                    increasing++;
+                }
+            }
+            if (increasing == grid.length) count++;
+        }
+        return count;
+    }
+
+    public String toString(){
+        String s = "";
+        for (int[] row : grid){
+            for (int col : row){
+                s += col + " ";
+            }
+            s += "\n";
+        }
+        return s;
     }
 
 
